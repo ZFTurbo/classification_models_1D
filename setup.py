47ee540436d2fe3fe03993e780e8fcba8f56fb7e
ZFTurbo <ZF-Turbo@yandex.ru>
2022-04-27 15:16:31 +0300

    setup.py added

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='classification_models_1D',
+    version='1.0.0',
+    author='Roman Solovyev (ZFTurbo)',
+    packages=['classification_models_1D', 'classification_models_1D/models'],
+    url='https://github.com/ZFTurbo/classification_models_1D',
+    description='Set of models for classification of 1D data.',
+    long_description='1D variants of popular CNN models for classification like ResNets, DenseNets, VGG, etc. '
+                     'It also contains weights obtained by converting ImageNet weights from the same 2D models (soon).'
+                     'More details: https://github.com/ZFTurbo/classification_models_1D',
+)
